939c94bf5effc06b8e110d0e22161aa0b0d300d4
net_bar_user <netbar@changsha.com>
2025-04-06 23:47:25 +0800

    last push

--- 0_debug.sh
+++ 0_debug.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 python main.py \
 --work_type Contrastive \
 --epochs 100 --bs 6 --lr 0.005  --num_workers 4\
---model_name contrastive_model1 \
+--model_name contrastive_model2 \
 --task NCvsPD  \
 --debug 1 \
 --pick_metric_name accuracy \
--- 1_run.sh
+++ 1_run.sh
@@ -1,6 +1,7 @@
 export CUDA_VISIBLE_DEVICES=1,2,3
-tasks=("NCvsPD" "NCvsProdromal" "ProdromalvsPD")
-model="contrastive_model1"
+#tasks=("NCvsPD" "NCvsProdromal" "ProdromalvsPD")
+tasks=("NCvsProdromal")
+model="contrastive_model2"
 for task in "${tasks[@]}"; do
   python main.py \
   --work_type Contrastive \
@@ -8,6 +9,6 @@ for task in "${tasks[@]}"; do
   --model_name "$model" --task "$task" \
   --debug 0 \
   --pick_metric_name accuracy \
-  --val_start 50 --val_interval 1 \
+  --val_start 1 --val_interval 1 \
   --early_stop_start 50 --patience 10
 done
